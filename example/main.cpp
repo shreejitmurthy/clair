93c239fc592620c8b0680a5eedea1867bddd5e54
Shreejit Murthy <shreejitmurthy@gmail.com>
2026-02-06 03:50:34 +1030

    update CMake configuration and add example implementation

---
+++ example/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "../clair.hh"
+
+void name_response(std::string v) {
+    std::cout << "Hello, " << v << std::endl;
+}
+
+int main(int argc, char** argv) {
+    clair cl("My CLI Program");
+
+    cl.exec("./main");
+    cl.version("0.1.0");
+    cl.description("The description to my very awesome CLI program.");
+    
+    cl.flag("name", name_response, "n");
+
+    cl.parse(argc, argv);
+
+    return 0;
+}
