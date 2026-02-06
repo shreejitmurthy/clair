fad0c96c51cb2e76bf1919364ef4ee6256d47255
Shreejit Murthy <shreejitmurthy@gmail.com>
2026-02-06 17:46:11 +1030

    update example

--- example/main.cpp
+++ example/main.cpp
@@ -13,8 +13,9 @@ int main(int argc, char** argv) {
     cl.version("0.1.0");
     cl.description("The description to my very awesome CLI program.");
     cl.short_description("This is my CLI program.");
+    cl.notes("This is just an example, there is more to be done with clair!");
     
-    cl.flag("name", name_response, "n");
+    cl.flag("name", name_response, "n", "Provide your name so we can say hello!");
 
     cl.parse(argc, argv);
 
